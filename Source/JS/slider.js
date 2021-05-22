99f557b7fef688ca9f7309621f2ed1cba085b3d2
CodeMogule <codemogule@gmail.com>
2021-05-22 16:29:27 +0300

    Marvel Api Web Dev

---
+++ Source/JS/slider.js
@@ -0,0 +1,40 @@
+ const slider = document.querySelectorAll('.slider-custom')
+ const dots = document.querySelectorAll('.dot')
+
+ let counter = 1
+ 
+
+ sliderWipe(counter)
+
+ function addSlider(x){
+     counter+=x
+     sliderWipe(counter)
+ } 
+
+ function currslider(x){
+     counter = x
+     sliderWipe(counter)
+ }
+
+ function sliderWipe(x){
+     let i;
+
+     for(i=0;i<slider.length;i++){
+         slider[i].style.display = 'none'
+     }
+
+     for(i=0;i<dots.length;i++){
+         dots[i].classList.remove('active')
+     }
+
+     if(x>slider.length){
+         counter = 1
+     }
+
+     else if (x<1){
+         counter = slider.length
+     }
+
+     slider[counter - 1].style.display = 'block'
+     dots[counter - 1].classList.add('active')
+ }
