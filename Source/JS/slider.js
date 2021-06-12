7bfecccc12f0bd336b0895066db17de9227abe81
CodeMogule <codemogule@gmail.com>
2021-06-12 15:07:25 +0300

    Marvel Api Web Dev

--- Source/JS/slider.js
+++ Source/JS/slider.js
@@ -1,47 +1,85 @@
- const slider = document.querySelectorAll('.slider-custom')
- const dots = document.querySelectorAll('.dot')
-
- let counter = 1
+ const elements = {
+     slider: document.querySelectorAll('.slider-custom'),
+     dots: document.querySelectorAll('.dot'),
+     sliderCharacter: document.querySelectorAll('.img-item-container')
+ };
  
+ 
+ 
+ function Slider(){
+    this.counter = 1; 
+    this.addSlider = function(x){
+        this.counter+=x;
+        this.sliderWipe(this.counter);
+    };
+    this.addSlideCharacter = function(x){
+        this.counter+=x;
+        this.sliderWipeCharacters(this.counter);
+    };
+    
+    this.currslider = function(x){
+        this.counter = x;
+        this.sliderWipe(this.counter);
+    }
+    this.sliderWipe = function(x){
+        let i;
 
- sliderWipe(counter)
+        //header
+     for(i=0;i<elements.slider.length;i++){
+         elements.slider[i].style.display = 'none';
+     };
 
- let timer = setInterval(automaticSlide,9000)
+     
+     for(i=0;i<elements.dots.length;i++){
+         elements.dots[i].classList.remove('active');
+     };
 
- function automaticSlide(){
-counter+=1
-sliderWipe(counter)
- }
-
- function addSlider(x){
-     counter+=x
-     sliderWipe(counter)
- } 
-
- function currslider(x){
-     counter = x
-     sliderWipe(counter)
- }
-
- function sliderWipe(x){
-     let i;
-
-     for(i=0;i<slider.length;i++){
-         slider[i].style.display = 'none'
-     }
-
-     for(i=0;i<dots.length;i++){
-         dots[i].classList.remove('active')
-     }
-
-    if(x>slider.length){
-         counter = 1
+     //header
+    if(x>elements.slider.length){
+         this.counter = 1;
      }
 
      else if (x<1){
-         counter = slider.length
+         this.counter = elements.slider.length;
      }
 
-     slider[counter - 1].style.display = 'block'
-     dots[counter - 1].classList.add('active')
+     elements.slider[this.counter - 1].style.display = 'block';
+     elements.dots[this.counter - 1].classList.add('active');
+    }
  }
+
+
+ 
+ Slider.prototype.sliderWipeCharacters = function(x){
+     let i;
+     //characters section
+     for(i=0;i<elements.sliderCharacter.length;i++){
+        elements.sliderCharacter[i].style.display = 'none';
+    };
+
+     //character section 
+     if(x>elements.sliderCharacter.length){
+        this.counter = 1;
+    }
+
+    else if (x<1){
+        this.counter = elements.sliderCharacter.length;
+    }
+
+    elements.sliderCharacter[this.counter - 1].style.display = 'block';
+    elements.sliderCharacter[this.counter - 1].style.display = 'flex';
+ }
+
+ 
+
+ let headerSlider = new Slider()
+ headerSlider.sliderWipe(this.counter);
+
+ let timer = setInterval(autoSlide,9000)
+function autoSlide (){
+    headerSlider.counter+=1;
+    headerSlider.sliderWipe(headerSlider.counter)
+}
+
+let characterSlider = new Slider();
+characterSlider.sliderWipeCharacters(this.counter)
